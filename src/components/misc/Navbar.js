8e020f0f0ce7b5f9768fcd0b9999b7c66c71bb86
gaurav-b <sunymca619@gmail.com>
2023-05-10 01:41:55 +0530

    adding / link to ezMaid text in navbar

--- src/components/misc/Navbar.js
+++ src/components/misc/Navbar.js
@@ -51,7 +51,7 @@ function Navbar() {
     <>
       <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
         <div className='container-fluid'>
-          <span className='navbar-brand'>ezMaid</span>
+          <span className='navbar-brand'><NavLink className='nav-link' to='/'>ezMaid</NavLink></span>
           <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
             aria-controls='navbarNavDropdown' aria-expanded='false'
             aria-label='Toggle Navigation'>
